b1b574ef5ea4d6f89456a6574dc4c46764ea175a
santiago rodriguez <9152404+santiagorodriguez-dev@users.noreply.github.com>
2024-10-25 18:18:59 +0200

    pool

--- src/soporte_scrap_web.py
+++ src/soporte_scrap_web.py
@@ -1,14 +1,18 @@
 # Importamos las librerías que necesitamos
-# Beautifulsoup
-from bs4 import BeautifulSoup
 
-# Requests
-import requests
+# Librerías de extracción de actividades
+# -----------------------------------------------------------------------
+from bs4 import BeautifulSoup # type: ignore
+import requests # type: ignore
 
-import pandas as pd
+# Tratamiento de actividades
+# -----------------------------------------------------------------------
+import pandas as pd # type: ignore
 import numpy as np
-
+import re
 from time import sleep
+import time
+import multiprocessing
 
 # Importar librerías para automatización de navegadores web con Selenium
 # -----------------------------------------------------------------------
@@ -21,16 +25,16 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException # Excepciones comunes de selenium que nos podemos encontrar
 from selenium.webdriver.common.by import By
 
-def get_urls(url, texto):
+def get_urls(input_data):
     driver = webdriver.Chrome()
     urls_return = []
-
+    url=input_data[0]
     driver.get(url)
     driver.maximize_window()
     sleep(5)
     driver.find_element('css selector','#rcc-confirm-button').click()
     sleep(5)
-    links = driver.find_elements(By.PARTIAL_LINK_TEXT, texto)
+    links = driver.find_elements(By.PARTIAL_LINK_TEXT, input_data[1])
 
     for i in links:
         urls_return.append(i.get_attribute("href"))
@@ -38,3 +42,22 @@ def get_urls(url, texto):
     driver.close()
 
     return urls_return
+
+def main(input_data, texto):
+
+    start_time = time.time()
+
+    lista_total = []
+    
+    args = [(i, texto) for i in input_data]
+
+    with multiprocessing.Pool(processes=multiprocessing.cpu_count()) as pool:
+        resultados = pool.map(get_urls, args)
+
+    for res in resultados:
+        lista_total.append(res)
+   
+    end_time = time.time()
+    print(f"\n el total duró {end_time - start_time:.2f} segundos.")
+    
+    return lista_total
