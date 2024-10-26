a34897fdac47459bd88210b3ec546bb4fcd1ae5c
santiago rodriguez <9152404+santiagorodriguez-dev@users.noreply.github.com>
2024-10-26 19:15:01 +0200

    todos los datos de productos.

--- src/soporte_scrap_web.py
+++ src/soporte_scrap_web.py
@@ -8,11 +8,13 @@ import requests # type: ignore
 # Tratamiento de actividades
 # -----------------------------------------------------------------------
 import pandas as pd # type: ignore
-import numpy as np
+import numpy as np # type: ignore
 import re
 from time import sleep
 import time
 import multiprocessing
+import asyncio
+from datetime import datetime
 
 # Importar librerías para automatización de navegadores web con Selenium
 # -----------------------------------------------------------------------
@@ -24,6 +26,10 @@ from selenium.webdriver.support.ui import WebDriverWait # type: ignore
 from selenium.webdriver.support import expected_conditions as EC # type: ignore
 from selenium.common.exceptions import NoSuchElementException # type: ignore # Excepciones comunes de selenium que nos podemos encontrar
 from selenium.webdriver.common.by import By # type: ignore
+
+
+supermercados_dicc = {'mercadona': 1, 'carrefour': 2, 'eroski': 3, 'dia': 4, 'hipercor':5, 'alcampo':6}
+categorias_dicc = {'aceite de girasol': 1, 'aceite de oliva': 2, 'leche': 3}
 
 def get_urls(input_data):
     driver = webdriver.Chrome()
@@ -49,7 +55,7 @@ def procesar(input_data, texto):
     
     args = [(i, texto) for i in input_data]
 
-    with multiprocessing.Pool(processes=4) as pool:
+    with multiprocessing.Pool(multiprocessing.cpu_count()) as pool:
         resultados = pool.map(get_urls, args)
 
     for res in resultados:
@@ -57,20 +63,18 @@ def procesar(input_data, texto):
    
     return retun_list
 
-def main(url_first):
+def get_list_productos(url_first):
 
     start_time = time.time()
 
     lista_all_productos = []
 
-    url_first = ["https://super.facua.org/"]
-
     list_urls_super = procesar(url_first,"Acceder")
 
     list_con_categorias=[]
 
-    for super in list_urls_super:
-        list_con_categorias.append(procesar(super,'Ver'))
+    for supermecados in list_urls_super:
+        list_con_categorias.append(procesar(supermecados,'Ver'))
 
     for k in list_con_categorias:
         for h in k:
@@ -81,3 +85,106 @@ def main(url_first):
     print(f"\n Total scrapeo de urls duró {end_time - start_time:.2f} segundos.")
 
     return lista_all_productos
+
+async def get_historico(url):
+
+    df_historico_producto = pd.DataFrame()
+   
+    url_sopa = url
+
+    res = requests.get(url_sopa)
+    sopa = BeautifulSoup(res.content, "html.parser")
+
+    resultado = sopa.find("tbody")
+    result_tr = resultado.findAll("tr")
+    
+    if (res.status_code == 200 and resultado and result_tr):
+        datos = url.split("/")[3:6]
+        supermercado = ''
+        categoria = ''
+        desc_producto = datos[2]
+
+        for x, y in supermercados_dicc.items():
+            if (str(datos[0]).replace('-',' ') == x):
+                supermercado = y
+        
+        for x, y in categorias_dicc.items():
+            if (str(datos[1]).replace('-',' ') == x):
+                categoria = y
+
+        lista_historico = []
+        
+        for tr in result_tr:
+
+            lista = tr.findAll("td")
+            lista_inter = []
+
+            for td in lista:
+                lista_inter.append(td.text)
+            
+            lista_inter.extend([supermercado,categoria,desc_producto])
+            lista_historico.append(lista_inter)
+
+    df_historico_producto = pd.DataFrame(lista_historico)
+
+    return df_historico_producto
+
+async def load_datos_historico(lista_all_productos):
+
+    start_time = time.time()
+
+    df = pd.DataFrame()
+
+    tareas = []
+
+    for sup in lista_all_productos:
+        for cat in sup:
+            for prod in cat:
+                tareas.append(get_historico(prod))
+
+    lista_resultado_df = await asyncio.gather(*tareas)
+
+    for i, r in enumerate(lista_resultado_df, 1):
+            df = pd.concat([df, r])
+            df.reset_index(drop=True, inplace=True)
+
+    end_time = time.time()
+    print(f"\n Total scrapeo de productos duró {end_time - start_time:.2f} segundos.")
+
+    return df
+
+def save_to_file(df):
+    print(f"Numero de registros: {df.shape[0]}")
+    fecha_hoy = datetime.today().strftime('%Y-%m-%d')
+    df.to_csv(f"../data/all_products_{fecha_hoy}.csv")
+    print(f"Fichero Salvado: '../data/all_products_{fecha_hoy}.csv'") 
+
+
+async def main():
+
+    df = pd.DataFrame()
+
+    lista_all_productos = get_list_productos(["https://super.facua.org/"])
+
+    df = await load_datos_historico(lista_all_productos)
+
+    df = df.rename(columns = {0: 'fecha', 
+                    1: 'precio',
+                    2: 'Variacion',
+                    3: 'id_supermercado',
+                    4: 'id_categoria',
+                    5: 'desc_producto'})
+    
+    save_to_file(df)
+
+    return df
+
+if __name__ == "__main__":
+    start_time = time.time()
+    asyncio.run(main())
+    end_time = time.time()
+    print(f"\n Tiempo total del proceso: {end_time - start_time:.2f} segundos.")
+
+
+
+
